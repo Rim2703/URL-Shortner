6c1b6bba37fcb0be116803af3f0684fa3017b791
Rimsha <khanrimsha976@gmail.com>
2022-10-07 13:14:28 +0530

    Final Update

--- src/Controllers/urlController.js
+++ src/Controllers/urlController.js
@@ -38,10 +38,12 @@ const isValid = function (value) {
     return true;
 }
 
+// ___________Validation for long URL_______________
 const isValidURL = function(URL){
     return /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%.\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%\+.~#?&//=]*)/.test(URL)
 }
 
+// _______________________create URL___________________________
 const createURL = async function (req, res) {
     try {
         let data = req.body
@@ -49,16 +51,25 @@ const createURL = async function (req, res) {
         if (Object.keys(data).length == 0) return res.status(400).send({ status: false, message: "Please Provide data" })
         if (!isValid(longUrl)) return res.status(400).send({ status: false, message: "Please provide URL" })
 
+        // ____________________validation testing for long URL_____________________
         if (!isValidURL(longUrl)) return res.status(400).send({ status: false, message: "Please provide valid URL" })
+       
+        // setting data from  DB to cache for same URL
+        let isUrlPresent = await urlModel.findOne({longUrl})
+        if(isUrlPresent){
+            console.log("db call..")
+            await SET_ASYNC(`${longUrl}`, JSON.stringify(isUrlPresent))
+            return res.status(200).send({status:true, message: "Short URL already genreated", data:isUrlPresent})
+        }
 
-        let cachedURLData = await GET_ASYNC(`${longUrl}`)
-        if (cachedURLData) return res.status(200).send({ status: true, message: "URL already shortened", data: JSON.parse(cachedURLData) })
-
+        // +++++++++++++++generating urlcode by using shortid package++++++++++++++++
         const urlCode = shortid.generate().toLowerCase()
 
+        // if By chance urlcode already exist 
         let alreadyURl = await urlModel.findOne({ urlCode: urlCode })
         if (alreadyURl) return res.status(409).send({ status: true, message: "URL already exist" })
 
+        // ____________generating short URL____________
         let shortUrl = baseUrl + urlCode
         let Data = {
             "longUrl": longUrl,
@@ -66,31 +77,36 @@ const createURL = async function (req, res) {
             "urlCode": urlCode
         }
 
+        // creating document
         await urlModel.create(Data)
-        let findUrl = await urlModel.findOne({longUrl: longUrl})
-        await SET_ASYNC(`${longUrl}`, JSON.stringify(findUrl))
         res.status(201).send({ status: true, message: "URL shortened Successfully", data: Data })
-
     }
     catch (err) {
         res.status(500).send({ status: false, message: err.message })
     }
 }
 
-
+// _____________________get URL__________________________
 const getUrl = async function (req, res) {
     try {
         let data = req.params.urlCode
 
+        // ___________validation for urlcode_____________
         if (!shortid.isValid(data)) return res.status(400).send({ status: false, message: "please enter valid URL code" })
 
+        // ______________recieving data from cache reduces the response time_____________
         let cachedURLData = await GET_ASYNC(`${data}`)
         if (cachedURLData) {
+            console.log("cache HIT")
           return res.status(302).redirect(JSON.parse(cachedURLData))
 
         } else {
+            console.log("cache MISS")
+            // find urlcode in db
             let findUrl = await urlModel.findOne({ urlCode: data })
             if (!findUrl) return res.status(404).send({ status: false, message: "UrlCode does not found!!" })
+           
+            // set longurl(Original) in cache with there respective key i.e urlcode
             await SET_ASYNC(`${data}`, JSON.stringify(findUrl.longUrl))
             return res.status(302).redirect(findUrl.longUrl)
         }
--- src/routers/route.js
+++ src/routers/route.js
@@ -2,10 +2,14 @@ const express = require('express')
 const router = express.Router()
 const urlController = require('../Controllers/urlController')
 
+// ___________________URL Shorten API____________________
 router.post("/url/shorten", urlController.createURL)
+
+// ___________________Original URL API_____________________
 router.get("/:urlCode", urlController.getUrl)
 
 
+//____________________additional api for testing router path____________________________
 router.all("/**", function(req,res){
     return res.status(404).send({status: false, message:"Make sure your endpoint currect or not"})
 })
